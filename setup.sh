1d2843e332ed650f5e0ecff43a65dcd919771ba0
Anastasiq Pchelina <gbroccol@mi-j3.msk.21-school.ru>
2020-12-02 17:50:30 +0300

    new

--- setup.sh
+++ setup.sh
@@ -21,52 +21,47 @@ kubectl apply -f ./srcs/metallb-config.yaml
 # eval $(minikube -p minikube docker-env)
 eval $(minikube docker-env)
 
-
 # nginx container
 printf "\n>> Nginx is building... \n"
 docker build -t nginx_image ./srcs/nginx
 echo "Done. "
 kubectl apply -f ./srcs/nginx/nginx.yaml
 
-                            # kubectl get svc
-                            # kubectl delete svc nginx-service
-                            # kubectl get deploy
-                            # kubectl delete deploy nginx-deployment
+                                # kubectl get svc
+                                # kubectl delete svc nginx-service
+                                # kubectl get deploy
+                                # kubectl delete deploy nginx-deployment
 
+# mysql container
+printf "\n>> Mysql is building... \n"
+docker build -t mysql_image ./srcs/mysql
+echo "Done. "
+kubectl apply -f ./srcs/mysql/mysql.yaml
+
+# wordpress container
+printf "\n>> Wordpress is building ... \n"
+docker build -t wp_image ./srcs/wordpress
+echo "Done. "
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
+
+# phpmyadmin container
+printf "\n>> PhpMyAdmin is building ... \n"
+docker build -t php_image ./srcs/phpmyadmin
+echo "Done. "
+kubectl apply -f ./srcs/phpmyadmin/php.yaml
 
 minikube dashboard
-# mysql container
-# printf "\n>> MySQL is building ... \n"
-# docker build -t mysql ./srcs/mysql > /dev/null
-# echo "Done. "
-# kubectl apply -f ./srcs/yaml-storage/mysql-storage.yaml
-# kubectl apply -f ./srcs/yaml-deploy/mysql-deploy.yaml
-# kubectl apply -f ./srcs/yaml-service/mysql-service.yaml
-
-# # wordpress container
-# printf "\n>> Wordpress is building ... \n"
-# docker build -t wordpress ./srcs/wordpress > /dev/null
-# echo "Done. "
-# kubectl apply -f ./srcs/yaml-deploy/wordpress-deploy.yaml
-# kubectl apply -f ./srcs/yaml-service/wordpress-service.yaml
-
-# # phpmyadmin container
-# printf "\n>> PhpMyAdmin is building ... \n"
-# docker build -t phpmyadmin ./srcs/phpmyadmin > /dev/null
-# echo "Done. "
-# kubectl apply -f ./srcs/yaml-deploy/phpmyadmin-deploy.yaml
-# kubectl apply -f ./srcs/yaml-service/phpmyadmin-service.yaml
 
 # # ftps container
 # printf "\n>> Ftps is building ... \n"
-# docker build -t ftps ./srcs/ftps > /dev/null
+# docker build -t ftps ./srcs/ftps
 # echo "Done. "
 # kubectl apply -f ./srcs/yaml-deploy/ftps-deploy.yaml
 # kubectl apply -f ./srcs/yaml-service/ftps-service.yaml
 
 # # influxdb container
 # printf "\n>> InfluxDB is building ... \n"
-# docker build -t influxdb ./srcs/influxdb > /dev/null
+# docker build -t influxdb ./srcs/influxdb
 # echo "Done. "
 # kubectl apply -f ./srcs/yaml-storage/influxdb-storage.yaml
 # kubectl apply -f ./srcs/yaml-deploy/influxdb-deploy.yaml
@@ -74,13 +69,13 @@ minikube dashboard
 
 # # telegraf metrics collector container
 # printf "\n>> Telegraf is building ... \n"
-# docker build -t telegraf ./srcs/telegraf > /dev/null
+# docker build -t telegraf ./srcs/telegraf
 # echo "Done. "
 # kubectl apply -f ./srcs/telegraf/telegraf-daemon.yaml
 
 # # grafana container
 # printf "\n>> Grafana is building ... \n"
-# docker build -t grafana ./srcs/grafana > /dev/null
+# docker build -t grafana ./srcs/grafana
 # echo "Done. "
 # kubectl apply -f ./srcs/yaml-deploy/grafana-deploy.yaml
 # kubectl apply -f ./srcs/yaml-service/grafana-service.yaml
---
+++ srcs/new/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+openrc default
+
+#/etc/init.d/mariadb setup
+#rc-service mariadb start 
+# mysql -u root mysql < /etc/mysql_conf.sql;
+# mysql -u root wordpress  < /etc/wordpress.sql ;
+#rc-service mariadb stop ;
+#/usr/bin/mysqld_safe ;
+
+sleep infinity
