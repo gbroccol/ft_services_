6c6a417ac6c71c79f0dfdae3ba3f0a455777a0dd
Anastasiq Pchelina <gbroccol@mi-h5.msk.21-school.ru>
2020-12-08 19:47:21 +0300

    PhpMyAdmin running

--- srcs/phpmyadmin/start.sh
+++ srcs/phpmyadmin/start.sh
@@ -2,11 +2,10 @@
 
 openrc default
 
-#/etc/init.d/mariadb setup
-#rc-service mariadb start 
-# mysql -u root mysql < /etc/mysql_conf.sql;
-# mysql -u root wordpress  < /etc/wordpress.sql ;
-#rc-service mariadb stop ;
-#/usr/bin/mysqld_safe ;
+mkdir www/phpmyadmin/tmp
+chmod 777 www/phpmyadmin/tmp
 
-sleep infinity
+mkdir /run/nginx
+chmod 744 /run/nginx
+
+/usr/bin/supervisord -c /etc/supervisord.conf
