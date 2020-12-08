0cace3e43be2cf55e4a21966e295a8b7ffd73989
Anastasiq Pchelina <gbroccol@mi-h5.msk.21-school.ru>
2020-12-08 19:47:44 +0300

    wordpress running

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -2,11 +2,9 @@
 
 openrc default
 
-#/etc/init.d/mariadb setup
-#rc-service mariadb start 
-# mysql -u root mysql < /etc/mysql_conf.sql;
-# mysql -u root wordpress  < /etc/wordpress.sql ;
-#rc-service mariadb stop ;
-#/usr/bin/mysqld_safe ;
+mkdir /run/nginx
+chmod 744 /run/nginx
 
-sleep infinity
+/usr/bin/supervisord -c /etc/supervisord.conf
+
+# sleep infinity
